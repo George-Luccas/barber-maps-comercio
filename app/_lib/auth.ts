5b38de7ba2e1b4cae24bf5318c51f64e475ed5f5
George-Luccas <georgeluccas300@gmail.com>
2025-12-29 20:55:13 -0400

    Fix: Completely decouple auth.ts from Prisma and Bcrypt to test NextAuth stability

--- app/_lib/auth.ts
+++ app/_lib/auth.ts
@@ -1,8 +1,8 @@
 import NextAuth from "next-auth";
-import { PrismaAdapter } from "@auth/prisma-adapter";
-import { db } from "./prisma";
+// import { PrismaAdapter } from "@auth/prisma-adapter";
+// import { db } from "./prisma";
 import Credentials from "next-auth/providers/credentials";
-import bcrypt from "bcryptjs";
+// import bcrypt from "bcryptjs";
 import { authConfig } from "@/app/auth.config";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
@@ -29,20 +29,22 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
 
             if (!credentials?.email || !credentials?.password) return null;
 
-            const user = await db.user.findUnique({
-              where: { email: credentials.email as string },
-            });
+            return null; // Forçando falha para quem não é Admin por enquanto
 
-            if (!user || !user.password) return null;
+            // const user = await db.user.findUnique({
+            //   where: { email: credentials.email as string },
+            // });
 
-            const isValid = await bcrypt.compare(
-              credentials.password as string,
-              user.password
-            );
+            // if (!user || !user.password) return null;
 
-            if (!isValid) return null;
+            // const isValid = await bcrypt.compare(
+            //   credentials.password as string,
+            //   user.password
+            // );
 
-            return user;
+            // if (!isValid) return null;
+
+            // return user;
         } catch (error) {
             console.error("Erro na autorização:", error);
             return null;
